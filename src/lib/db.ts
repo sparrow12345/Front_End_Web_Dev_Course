867b940529e25445dfa3fc3cef9798dda34abc78
sparrow12345 <m.naser@innopolis.university>
2024-03-22 01:25:15 +0300

    it all ends here

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -12,7 +12,6 @@ const globalForPrisma = globalThis as unknown as {
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-// if (process.env.NODE_ENV !== "production")
-globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export const db = prisma;
